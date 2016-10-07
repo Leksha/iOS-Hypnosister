9b0e799ef8e1e8e5adff28498be96c95f1757ee9
Leksha Ramdenee <leksha.ramdenee@tulip.io>
2016-10-07 10:36:13 -0400

    Initial Commit

---
+++ Hypnosister2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister2
+//
+//  Created by Leksha Ramdenee on 2016-10-07.
+//  Copyright © 2016 Leksha Ramdenee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister2
+//
+//  Created by Leksha Ramdenee on 2016-10-07.
+//  Copyright © 2016 Leksha Ramdenee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
